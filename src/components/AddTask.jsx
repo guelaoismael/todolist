ee8ed1391ca3f5595b10f4bf1363d417180ea196
guelaoismael <guelaoismael@gmail.com>
2025-02-20 14:37:53 +0100

    chore(#4): Add AddTask component, fixes #4

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,23 @@
+export const AddTask = () => {
+  return (
+    <div className="mt-8 grid  place-items-center">
+      <div className=" p-6 w-180 ">
+        <form action="">
+          <div className=" mt-2 ">
+            <div className="flex gap-x-4">
+            <div className=" flex flex-grow items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-blue-600">
+              <input
+              placeholder="Add task"
+                type="text"
+                className="  min-w-0 flex-auto py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+              />
+            </div>
+            <button className="flex-none rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-blue-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500">Ajouter</button>
+            </div>
+                      
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
