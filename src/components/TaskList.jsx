641520dc118639c536f835f21ea5e0e6fc580e7d
guelaoismael <guelaoismael@gmail.com>
2025-02-20 19:57:12 +0100

    feat(#10): Implement clear all task funtionality, fixes #10

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,8 +1,10 @@
 import { TaskCard } from "./TaskCard";
 
-
-export const TaskList = ({taskList, setTaskList}) => {
-    
+export const TaskList = ({ taskList, setTaskList }) => {
+    const clearAll = () => {
+        
+        setTaskList([]);
+    }
   return (
     <div className="mt-8 shadow-md p-4 bg-gray-50/2">
       <div className="flex justify-between">
@@ -13,16 +15,17 @@ export const TaskList = ({taskList, setTaskList}) => {
           </span>
         </div>
         <div>
-          <button className="flex-none rounded-md bg-red-500 p-1.5 text-sm font-semibold text-white shadow-xs hover:bg-red-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500">
+          <button 
+          onClick={clearAll}
+          className="flex-none rounded-md bg-red-500 p-1.5 text-sm font-semibold text-white shadow-xs hover:bg-red-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500">
             Tout Supprimer
           </button>
         </div>
       </div>
       <div className="mt-6 grid grid-cols-3 gap-4">
-        {taskList.map((task)=> (
-            <TaskCard key={task.id} task={task} />
+        {taskList.map((task) => (
+          <TaskCard key={task.id} task={task} />
         ))}
-        
       </div>
     </div>
   );
