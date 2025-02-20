328cb8510c47bf96f523b5d3bd8cb6fe373f3ad7
guelaoismael <guelaoismael@gmail.com>
2025-02-20 18:31:45 +0100

    feat: Integrate TaskList into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Header } from './components/Header'
+import { AddTask } from './components/AddTask'
+import { TaskList } from './components/TaskList'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <h1 className="text-4xl">Hello world</h1>
+    <Header />
+    <AddTask />
+    <TaskList />
     </>
   )
 }
